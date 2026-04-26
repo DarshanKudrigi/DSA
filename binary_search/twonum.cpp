4eeb52adc8b77dd790a767df188799f6cdbb3fa4
DarshanKudrigi <darshankudrigi2005@gmail.com>
2026-04-26 23:57:30 +0530

    added binary search files

---
+++ binary_search/twonum.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        vector<int> ans;
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
