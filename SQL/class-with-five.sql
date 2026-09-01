e33f68dc0e204288dbba04704c0ee302c26de853
DarshanKudrigi <darshankudrigi2005@gmail.com>
2026-09-01 23:23:07 +0530

    Sql-question updated

--- SQL/class-with-five.sql
+++ SQL/class-with-five.sql
@@ -1,5 +1,41 @@
 // SQL query to find classes with at least 5 students enrolled
 
+// question leval: Easy
+// question type: SQL
+// question link: https://leetcode.com/problems/classes-with-at-least-5-students/
+
+// The Courses table has the following columns:
+// +-------------+---------+
+// | Column Name | Type    |
+// +-------------+---------+
+// | student     | varchar |
+// | class       | varchar |
+// +-------------+---------+
+
+
+exaxmple 1:
+Input:
++------------+---------+
+| student    | class   |
++------------+---------+
+| Alice      | Math    |
+| Bob        | Math    |
+| Charlie    | Math    |
+| David      | Math    |
+| Eve        | Math    |
+| Frank      | Science |
+| Grace      | Science |
+| Henry      | Science |
+| Irene      | Science |
+| Jack       | Science |
++------------+---------+
+
+Output:
++---------+
+| class   |
++---------+
+| Math    |
++---------+
 
 
 
