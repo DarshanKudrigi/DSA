a20e04c0a404e1cc12762efafb69204219e1f1df
DarshanKudrigi <darshankudrigi2005@gmail.com>
2026-09-21 23:27:42 +0530

    Sliding-window

--- arrays/sliding-window/Maximum Sum of Distinct.cpp
+++ arrays/sliding-window/Maximum Sum of Distinct.cpp
@@ -105,3 +105,45 @@ public:
         return ans;
     }
 };
+
+
+
+
+// 
+
+
+
+
+
+
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        int l = 0;
+        long long sum = 0;
+        long long ans = 0;
+        unordered_map<int, int> mp;
+
+        for (int r = 0; r < nums.size(); r++) {
+            sum += nums[r];
+            mp[nums[r]]++;
+            if (r - l + 1 > k) {
+                sum -= nums[l];
+                mp[nums[l]]--;
+
+                if (mp[nums[l]] == 0) {
+                    mp.erase(nums[l]);
+                }
+
+                l++;
+            }
+            if (r - l + 1 == k) {
+                if (mp.size() == k) {
+                    ans = max(ans, sum);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
